ed066d18931979f69c13855c83876b220d967ca9
KaitlinFeagan <57116886+KaitlinFeagan@users.noreply.github.com>
2021-08-29 22:51:32 -0400

    Add files via upload

---
+++ ex15.rb
@@ -0,0 +1,20 @@
+#takes in your file and puts it as a variable, filename
+filename = ARGV.first
+
+#opens the text file you specified
+txt = open(filename)
+
+#prints to the screen what was in the text file
+puts "Here's your file #{filename}:"
+print txt.read
+
+#has you do this again, assigns file to a second variable
+print "Type the filename again: "
+file_again = $stdin.gets.chomp
+
+#opens file again
+txt_again = open(file_again)
+
+
+#prints out the file that it just opened
+print txt_again.read
