0655dfe0bdf9e691d87316cd2a3c6b1dd288734e
KaitlinFeagan <57116886+KaitlinFeagan@users.noreply.github.com>
2021-09-01 17:43:59 -0400

    Add files via upload

---
+++ ex19.rb
@@ -0,0 +1,26 @@
+def cheese_and_crackers(cheese_count, boxes_of_crackers)
+    puts "You have #{cheese_count} cheeses!"
+    puts "You have #{boxes_of_crackers} boxes of crackers!"
+    puts "Man that's enough for a party!"
+    puts "Get a blanket.\n"
+end
+
+puts "We can just give the function numbers directly:"
+cheese_and_crackers(20,30)
+
+puts "OR, we can use variables from our script:"
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+puts "We can even do math inside too:"
+    cheese_and_crackers(10 + 20, 5 + 6)
+
+puts "And we can combine the two, variables and math:"
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
+
+puts "Testing a function, please give me a number."
+your_number = $stdin.gets.chomp
+your_int = your_number.to_i
+puts "You picked #{your_int}.  Your number plus ten would be #{your_int + 10}."
